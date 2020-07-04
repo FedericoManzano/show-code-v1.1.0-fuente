72d5cfd79b7474a84382c099f1064e8dd2d53f4f
FedericoManzano <federicomanzano111@gmail.com>
2020-07-04 14:39:14 -0300

    Js llamadas

--- src/modulos/CodigoJs.js
+++ src/modulos/CodigoJs.js
@@ -11,8 +11,24 @@ import $ from "jquery"
                 resultado += "<span class='show-string'>" + codigo[i] + "</span>"
                 i ++ 
                 while(i < codigo.length && codigo[i] !== car) {
-                    resultado += "<span class='show-string'>" + codigo[i] + "</span>"
-                    i ++
+                    if(car === '`') {
+                        if(codigo[i] === '$') {
+                            while(i < codigo.length && codigo[i] !== '}') {
+                                resultado += "<span class='show-res'>" + codigo[i] + "</span>"
+                                i ++
+                            }
+                            if(codigo[i] === '}') {
+                                resultado += "<span class='show-res'>" + codigo[i] + "</span>"
+                                i ++
+                            }
+                        } else {
+                            resultado += "<span class='show-string'>" + codigo[i] + "</span>"
+                            i ++
+                        }
+                    } else {
+                        resultado += "<span class='show-string'>" + codigo[i] + "</span>"
+                        i ++
+                    }
                 }
                 if(codigo[i] === car) {
                     resultado += "<span class='show-string'>" + codigo[i] + "</span>"
@@ -59,69 +75,29 @@ import $ from "jquery"
         return resultado
     }
 
-    const colrearJson = (codigo) => {
-        let met = /{[\t ]*\n*[\t ]*(\n*[\t ]*[a-zA-Z0-9]+(:[\t ]*"*.+"*)*,*[\t\n ]*)+ *[\t ]*\n*}/
-        let bus = codigo.search(met)
-        let pal = ""
-        let aux = codigo.substring(bus, codigo.length)
-        let i = 0
-        
-        let retorno = ""
-
-        while(bus !== -1) { 
-            pal = ""
-            retorno = ""
-
-
-            while(aux[i] !== '}' && i < aux.length) {
-                pal += aux[i]
-                i ++
-            }
-    
-            if(aux[i] === '}') {
-                pal += aux[i]
-                i ++
-            }
-
-            codigo = codigo.replace(pal, `<span class='show-json'>${pal}</span>`)
-            aux = aux.substring(i + 1, aux.length)
-           
-            bus = aux.search(met)
-            i = bus
-        }
-
-        return codigo
-    }
-
 
     const colorearNombre = (codigo) => {
+
+
         let met = /[a-z]+ *[a-zA-Z0-9\$-_]+ *= *\(.*\) *=&gt; *\n*{/
         let bus = codigo.search(met)
-        let pal = ""
         let aux = codigo.substring(bus, codigo.length)
         let i = 0
        
-
         while(bus !== -1) { 
-            pal = ""
+            let pal = ""
           
-            while(aux[i] !== ' ' && i < aux.length) {
+            while(aux[i] !== ' ' && i < aux.length) 
                 i ++
-            }
-
-            while(aux[i] === ' ' && i < aux.length) {
+            while(aux[i] === ' ' && i < aux.length) 
                 i ++
-            }
-           
             while(aux[i] !== '=') {
                 pal += aux[i]
                 i ++
             }
 
             codigo = codigo.replace(pal, `<span class='show-met'>${pal}</span>`)
-               
             aux = aux.substring(i + 1, aux.length)
-           
             bus = aux.search(met)
             i = bus
         }
@@ -133,37 +109,52 @@ import $ from "jquery"
     const colorearNombreFuncion = (codigo) => {
         let met = /function[\t ]+[a-zA-Z0-9-_\$]+[\t ]*\([\t ]*.*[\t ]*\)[\t ]*{/
         let bus = codigo.search(met)
-        let pal = ""
         let aux = codigo.substring(bus, codigo.length)
         let i = 0
        
-
         while(bus !== -1) { 
-            pal = ""
+            let pal = ""
           
-            while(aux[i] !== ' ' && i < aux.length) {
+            while(aux[i] !== ' ' && i < aux.length) 
                 i ++
-            }
-
-            while(aux[i] === ' ' && i < aux.length) {
+            while(aux[i] === ' ' && i < aux.length) 
                 i ++
-            }
-           
             while(aux[i] !== '(') {
                 pal += aux[i]
                 i ++
             }
 
             codigo = codigo.replace(pal, `<span class='show-met'>${pal}</span>`)
-               
             aux = aux.substring(i + 1, aux.length)
-           
             bus = aux.search(met)
             i = bus
         }
 
         return codigo
     }
+
+
+    const colorearLlamadas = (codigo) => {
+        let met = /\.[a-zA-Z0-9]+[\t ]*\(.*\)/
+        let bus = codigo.search(met)
+        let aux = codigo.substring(bus, codigo.length)
+        let i = 0
+       
+        while(bus !== -1) { 
+            let pal = ""
+            while(aux[i] !== '(') {
+                pal += aux[i]
+                i ++
+            }
+            codigo = codigo.replace(new RegExp(pal, "g"), `<span class='show-llamadas'>${pal}</span>`)
+            aux = aux.substring(i + 1, aux.length)
+            bus = aux.search(met)
+            i = bus
+        }
+
+        return codigo
+    }
+
 
     const inicializar = () => {
         $(".cod-js").each((index, e) => {
@@ -173,17 +164,19 @@ import $ from "jquery"
             
             let resultado = colorearCadenas(codigo)
             resultado = colorearComentarios(resultado)
-           //resultado = colrearJson(resultado)
             resultado = colorearNombre(resultado)
             resultado = colorearNombreFuncion(resultado)
+            resultado = colorearLlamadas(resultado)
            
+
             resultado = resultado.replace(/=&gt;/g, "<span class='show-res'>=></span>")
+            resultado = resultado.replace(/undefined/g, "<span class='show-res'>undefined</span>")
             resultado = resultado.replace(/const /g, "<span class='show-res'>const </span>")
             resultado = resultado.replace(/let /g, "<span class='show-res'>let </span>")
             resultado = resultado.replace(/var /g, "<span class='show-res'>var </span>")
             resultado = resultado.replace(/class /g, "<span class='show-res'>class </span>")
-            resultado = resultado.replace(/function /g, "<span class='show-res'>function </span>")
-            resultado = resultado.replace(/constructor /g, "<span class='show-res'>constructor </span>")
+            resultado = resultado.replace(/function/g, "<span class='show-res'>function</span>")
+            resultado = resultado.replace(/constructor/g, "<span class='show-res'>constructor</span>")
             resultado = resultado.replace(/this/g, "<span class='show-res'>this</span>")
             resultado = resultado.replace(/typeof/g, "<span class='show-res'>typeof</span>")
             resultado = resultado.replace(/import /g, "<span class='show-res'>import </span>")
@@ -193,46 +186,30 @@ import $ from "jquery"
             resultado = resultado.replace(/false/g, "<span class='show-res'>false </span>")
             resultado = resultado.replace(/new /g, "<span class='show-res'>new </span>")
 
-            resultado = resultado.replace(/if/g, "<span class='show-control'>if</span>")
+            resultado = resultado.replace(/if[\n ]*\(/g, "<span class='show-control'>if</span>&nbsp;<span class='show-neutro'>(</span>")
             resultado = resultado.replace(/else/g, "<span class='show-control'>else</span>")
-            resultado = resultado.replace(/while/g, "<span class='show-control'>while</span>")
+            resultado = resultado.replace(/while[\n ]*\(/g, "<span class='show-control'>while</span>&nbsp;<span class='show-neutro'>(</span>")
             resultado = resultado.replace(/switch/g, "<span class='show-control'>switch</span>")
             resultado = resultado.replace(/case/g, "<span class='show-control'>case</span>")
             resultado = resultado.replace(/default/g, "<span class='show-control'>default</span>")
             resultado = resultado.replace(/return/g, "<span class='show-control'>return </span>")
-            resultado = resultado.replace(/do /g, "<span class='show-control'>do </span>")
+            resultado = resultado.replace(/do[\n ]*{/g, "<span class='show-control'>do</span>&nbsp;<span class='show-neutro'>{</span>")
             resultado = resultado.replace(/forEach/g, "<span class='show-control'>forEach</span>")
-            resultado = resultado.replace(/for /g, "<span class='show-control'>for </span>")
+            resultado = resultado.replace(/for[\n ]*\(/g, "<span class='show-control'>for</span>&nbsp;<span class='show-neutro'>(</span>")
             resultado = resultado.replace(/break/g, "<span class='show-control'>break</span>")
 
 
             resultado = resultado.replace(/!/g, "<span class='show-op'>!</span>")
             
-
+            resultado = resultado.replace(/console\.log/g, "<span class='show-sistema'>console</span><span class='show-neutro'>.</span><span class='show-control'>log</span>")
+            resultado = resultado.replace(/console\.error/g, "<span class='show-sistema'>console</span><span class='show-neutro'>.</span><span class='show-error'>error</span>")
             resultado = resultado.replace(/console\./g, "<span class='show-sistema'>console</span><span class='show-neutro'>.</span>")
             resultado = resultado.replace(/window\./g, "<span class='show-sistema'>window</span><span class='show-neutro'>.</span>")
             resultado = resultado.replace(/\( *window *\)/g, "<span class='show-neutro'>( </span><span class='show-sistema'>window</span><span class='show-neutro'> )</span>")
             resultado = resultado.replace(/document\./g, "<span class='show-sistema'>document</span><span class='show-neutro'>.</span>")
             resultado = resultado.replace(/\( *document *\)/g, "<span class='show-neutro'>( </span><span class='show-sistema'>document</span><span class='show-neutro'> )</span>")
             resultado = resultado.replace(/alert\./g, "<span class='show-sistema'>alert</span><span class='show-neutro'>.</span>")
-            /*resultado = resultado.replace(/\(/g, "<span class='show-neutro'>(</span>")
-            resultado = resultado.replace(/\)/g, "<span class='show-neutro'>)</span>")
-            resultado = resultado.replace(/\+/g, "<span class='show-neutro'>+</span>")
-            resultado = resultado.replace(/{/g, "<span class='show-neutro'>{</span>")
-            resultado = resultado.replace(/}/g, "<span class='show-neutro'>}</span>")
-            resultado = resultado.replace(/&amp;&amp;/g, "<span class='show-neutro'>&&</span>")
-            resultado = resultado.replace(/\|/g, "<span class='show-neutro'>|</span>")
-            resultado = resultado.replace(/===/g, "<span class='show-neutro'>===</span>")
-            resultado = resultado.replace(/&lt;/g, "<span class='show-neutro'><</span>")
-            resultado = resultado.replace(/&lt;=/g, "<span class='show-neutro'>&lt=</span>")
-            resultado = resultado.replace(/&gt;/g, "<span class='show-neutro'>&gt</span>")
-            resultado = resultado.replace(/&gt;=/g, "<span class='show-neutro'>&gt=</span>")
-            resultado = resultado.replace(/[^show-]\-/g, "<span class='show-neutro'>-</span>")
-            resultado = resultado.replace(/[^class=]=/g, "<span class='show-neutro'>=</span>")*/
             
-
-
-
             
             $(e).html(resultado)
         })
