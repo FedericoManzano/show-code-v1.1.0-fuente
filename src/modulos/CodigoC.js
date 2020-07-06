eebd39449cb47b42dd43a806838dddeec8bb2709
FedericoManzano <federicomanzano111@gmail.com>
2020-07-06 04:50:27 -0300

    else agregado

--- src/modulos/CodigoC.js
+++ src/modulos/CodigoC.js
@@ -225,6 +225,7 @@ import numerar from "./Numeracion"
             resultado = resultado.replace(/while[\t ]*\(/g, "<span class='show-res'>while </span></span><span class='show-neutro'>(</span>")
             resultado = resultado.replace(/for[\t ]*\(/g, "<span class='show-res'>for </span></span><span class='show-neutro'>(</span>")
             resultado = resultado.replace(/do[\t\n ]*{/g, "<span class='show-res'>do </span></span><span class='show-neutro'>{</span>")
+            resultado = resultado.replace(/else /g, "<span class='show-res'>else </span>")
             
             resultado = resultado.replace(/case/g, "<span class='show-res'>case</span>")
             resultado = resultado.replace(/break/g, "<span class='show-res'>break</span>")
